7b707dfc0e60405ca8fd2070636bd43cd7b9815a
Seremba <pserembae@gmail.com>
2021-06-23 16:42:31 +0300

    grab current tab

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -4,11 +4,29 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+const tabs = [ {url: "https://www.linkedin.com/in/per-harald-borgen/"}]
+
+tabBtn.addEventListener("click", function(){
+    // grabbing current tab 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id // or do whatever you need
+   
+     })
+     
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
